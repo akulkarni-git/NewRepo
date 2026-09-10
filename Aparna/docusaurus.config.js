6c74689f07069bed94a2f3cff1d89080745a8828
akulkarni-git <aparna.a.saxena@gmail.com>
2026-09-10 16:31:02 +0530

    BaseURL change

--- Aparna/docusaurus.config.js
+++ Aparna/docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: 'https://akulkarni-git.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: 'akulkarni-git',
+  baseUrl: '/NewRepo/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
