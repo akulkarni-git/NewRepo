cffe9375664c50f9bad49ac18188d603b4c7d007
akulkarni-git <aparna.a.saxena@gmail.com>
2026-09-15 16:53:13 +0530

    Final

--- Aparna/docusaurus.config.js
+++ Aparna/docusaurus.config.js
@@ -10,8 +10,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Aparna's Site',
+  tagline: 'Dinosaurs are scary',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
@@ -83,7 +83,7 @@ const config = {
         respectPrefersColorScheme: true,
       },
       navbar: {
-        title: 'My Site',
+        title: 'My Site - by Aparna',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
